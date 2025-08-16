6fe3549892248660e03662954c40c448b8b0bcc5
amit7800 <1200amitkr1999@gmail.com>
2025-08-16 22:48:02 +0530

    added reservation form

--- src/app/reservation/reservation-form/reservation-form.ts
+++ src/app/reservation/reservation-form/reservation-form.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reservation-form.html',
   styleUrl: './reservation-form.css'
 })
-export class ReservationForm {
+export class ReservationForm implements OnInit {
+
+  reservationForm: FormGroup = new FormGroup({});
+
+  constructor(private formbuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.reservationForm = this.formbuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+
+    if(this.reservationForm.valid) {
+      console.log('valid');
+    }
+  }
 
 }
